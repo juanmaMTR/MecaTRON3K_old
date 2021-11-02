aedce135ed2b3a486f0f957fb77f70bf1e12a0e1
Miguel Jaque Barbero <mjaque@fundacionloyola.es>
2021-11-02 12:30:49 +0100

    Más

--- js/mecatron3k.js
+++ js/mecatron3k.js
@@ -26,13 +26,41 @@ class Juego{
     console.log('Iniciando...')
     this.divPrincipal = document.getElementById('divPrincipal')
     this.vista.div = this.divPrincipal
-    this.generadorPalabras = window.setInterval(this.generarPalabra.bind(this), 1000)
-    this.animador = window.setInterval(this.vista.moverPalabras.bind(this.vista), 100)
+    this.generadorPalabras = window.setInterval(this.generarPalabra.bind(this), 3000)
+    this.animador = window.setInterval(this.vista.moverPalabras.bind(this.vista), 300)
+    window.onkeypress = this.pulsar.bind(this)
   }
 
   generarPalabra(){
     let nuevaPalabra = this.modelo.crearPalabra()
     this.vista.dibujar(nuevaPalabra)
+  }
+
+  pulsar(evento){
+    let letraPulsada = evento.key
+    //Busco todas las palabras
+    let palabras = this.divPrincipal.querySelectorAll('.palabra')
+    for(let palabra of palabras){
+      let span = palabra.children.item(0)
+      let nodoTexto = palabra.childNodes[1]
+      let textoRestante = nodoTexto.nodeValue
+      let primeraLetraTextoRestante = textoRestante.charAt(0)
+      if (letraPulsada == primeraLetraTextoRestante){
+        span.textContent += letraPulsada
+        nodoTexto.nodeValue = textoRestante.substring(1)
+
+        //Si ha completado la palabra, la elimino y sumo puntos
+        if (nodoTexto.nodeValue.length == 0){
+          palabra.remove()
+          this.modelo.sumarPunto()
+        }
+      }
+      else{
+        //Ha fallado, repongo el texto de la palabra
+
+      }
+
+    }
   }
 }
 
@@ -41,7 +69,7 @@ class Juego{
 **/
 class Vista{
   constructor(){
-    this.div = null
+    this.div = null   //Div donde se desarrolla el juego
   }
   /**
     Dibuja el área de juego.
@@ -51,6 +79,8 @@ class Vista{
     // <div class=palabra>Meca</div>
     let div = document.createElement('div')
     this.div.appendChild(div)
+    let span = document.createElement('span')
+    div.appendChild(span)
     div.appendChild(document.createTextNode(palabra))
     div.classList.add('palabra')
     div.style.top = '0px'
